5e9614296f73d77a82b0598f12023efe9d8e2814
Andrew Reynolds <andrew_reynolds@mac.com>
2020-09-29 20:43:25 -0700

    passed gif response data as prop down to gif component

--- src/Components/Gif.js
+++ src/Components/Gif.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React from "react";
 
-const Gif = props => (
+const Gif = ({ url }) => (
   <li className="gif-wrap">
-    <img alt=""/>
+    <img src={url} alt="" />
   </li>
 );
 
--- src/Components/GifList.js
+++ src/Components/GifList.js
@@ -1,13 +1,9 @@
-import React from 'react';
-import Gif from './Gif';
+import React from "react";
+import Gif from "./Gif";
 
-const GifList = props => { 
-  
-  return(
-    <ul className="gif-list">
-      {/* <Gif /> */}
-    </ul> 
-  );
-}
+const GifList = ({ data }) => {
+  let gifs = data.map((item) => <Gif url={item.images.fixed_height.url} key={item.id} />);
+  return <ul className="gif-list">{gifs}</ul>;
+};
 
 export default GifList;
