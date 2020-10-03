d35a2e0153350f1e7e8a8fc9080a498f0aaec101
Andrew Reynolds <andrew_reynolds@mac.com>
2020-10-03 13:34:19 -0700

    removed old comments

--- src/App.js
+++ src/App.js
@@ -14,17 +14,6 @@ export default class App extends Component {
   }
 
   componentDidMount() {
-    // fetch(`http://api.giphy.com/v1/gifs/trending?api_key=${process.env.REACT_APP_GIF_API}`)
-    //   .then((response) => response.json())
-    //   .then((data) => this.setState({ gifs: data.data }))
-    //   .catch((error) => console.log("Error fetcing and parsing data...", error));
-    // Axios.get(`http://api.giphy.com/v1/gifs/trending?api_key=${process.env.REACT_APP_GIF_API}`)
-    //   .then((response) =>
-    //     this.setState({
-    //       gifs: response.data.data,
-    //     })
-    //   )
-    //   .catch((error) => console.log("Error fetching and parsing data...", error));
     this.performSearch();
   }
 
